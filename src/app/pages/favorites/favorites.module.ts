65f1dcbecac867d253b3f70dbee39da7f14de267
Serhii Hurko <serhii_hurko@epam.com>
2023-03-21 21:59:02 +0200

    fix: Added ripple effect for Favorites
    NOJIRA

--- src/app/pages/favorites/favorites.module.ts
+++ src/app/pages/favorites/favorites.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatRippleModule } from '@angular/material/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 
 import { FavoritesRoutingModule } from './favorites-routing.module';
@@ -8,6 +9,6 @@ import { FavoritesComponent } from './favorites.component';
 
 @NgModule({
   declarations: [FavoritesComponent],
-  imports: [CommonModule, FavoritesRoutingModule, MatGridListModule],
+  imports: [CommonModule, FavoritesRoutingModule, MatRippleModule, MatGridListModule],
 })
 export class FavoritesModule {}
